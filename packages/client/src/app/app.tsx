62f469053ac196e1ea56272adefec47cb5ab6cd0
Mairis <berzinsmairis@gmail.com>
2023-07-01 22:51:56 -0700

    add bootstrap

--- packages/client/src/app/app.tsx
+++ packages/client/src/app/app.tsx
@@ -1,8 +1,7 @@
-import styled from 'styled-components';
-
-import NxWelcome from './nx-welcome';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 
 const StyledApp = styled.div`
   // Your style here
@@ -11,44 +10,43 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="client" />
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Navbar
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/page-2">
+              Page 2
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+      <Container>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                This is the generated root route.{' '}
+                <Link to="/page-2">Click here for page 2.</Link>
+              </div>
+            }
+          />
+          <Route
+            path="/page-2"
+            element={
+              <div>
+                <Link to="/">Click here to go back to root page.</Link>
+              </div>
+            }
+          />
+        </Routes>
+      </Container>
     </StyledApp>
   );
 }
