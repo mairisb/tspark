cbc10770ab2f3dea77138ac74cff4cf75d949414
Mairis <berzinsmairis@gmail.com>
2023-07-06 10:06:14 -0700

    Use repository instead of entity manager

--- packages/server/src/services/user.service.ts
+++ packages/server/src/services/user.service.ts
@@ -1,8 +1,10 @@
 import appDataSource from '../config/app-data-source';
 import User from '../models/user.model';
 
+const userRepository = appDataSource.getRepository(User);
+
 const getAll = async () => {
-  return appDataSource.manager.find(User);
+  return userRepository.find();
 };
 
 const UserService = {
