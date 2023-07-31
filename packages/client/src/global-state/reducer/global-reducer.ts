88fa7c210e9658ae61b1d9abfd6a7a40dc057555
mairisb <berzinsmairis@gmail.com>
2023-07-31 11:05:14 -0700

    use enum instead of string value for action type

--- packages/client/src/global-state/reducer/global-reducer.ts
+++ packages/client/src/global-state/reducer/global-reducer.ts
@@ -1,13 +1,14 @@
 import { Reducer } from 'react';
 import { GlobalState } from '../global-state';
 import { GlobalReducerAction } from './global-reducer-action';
+import { GlobalReducerActionTypes } from './global-reducer-action-types';
 
 export const globalReducer: Reducer<GlobalState, GlobalReducerAction> = (
   state,
   action
 ) => {
   switch (action.type) {
-    case 'IS_AUTHORIZED':
+    case GlobalReducerActionTypes.IS_AUTHORIZED:
       return { ...state, isLoggedIn: action.payload };
     default:
       return state;
