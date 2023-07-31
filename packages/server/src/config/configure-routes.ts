2190871b8eb57be27cb9f44a827aa3fed9ea6f68
mairisb <berzinsmairis@gmail.com>
2023-07-31 12:53:11 -0700

    extract api prefix out of the route configurations

--- packages/server/src/config/configure-routes.ts
+++ packages/server/src/config/configure-routes.ts
@@ -1,10 +1,14 @@
-import { Application } from 'express';
+import express, { Application } from 'express';
 import { authRouter } from '../routers/auth.router';
 import { gameRouter } from '../routers/game.router';
 import { userRouter } from '../routers/user.router';
 
+const mainRouter = express.Router();
+
+mainRouter.use('/auth', authRouter);
+mainRouter.use('/users', userRouter);
+mainRouter.use('/games', gameRouter);
+
 export const configureRoutes = (app: Application) => {
-  app.use('/api/auth', authRouter);
-  app.use('/api/users', userRouter);
-  app.use('/api/games', gameRouter);
+  app.use('/api', mainRouter);
 };
