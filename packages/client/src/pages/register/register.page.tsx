3d653c8aadf850751c4c543b84f434a46f2fba8d
mairisb <berzinsmairis@gmail.com>
2023-08-04 15:35:22 -0700

    extract register form to auth/components

--- packages/client/src/pages/register/register.page.tsx
+++ packages/client/src/pages/register/register.page.tsx
@@ -1,64 +1,10 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, Form, Stack } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { authService } from '../../services/auth.service';
+import { RegisterForm } from '../../features/auth/components';
 import { Page } from '../page';
 
-interface RegistrationFormData {
-  email: string;
-  password: string;
-}
-
-const registrationFormSchema: yup.ObjectSchema<RegistrationFormData> =
-  yup.object({
-    email: yup.string().email().required(),
-    password: yup.string().min(8).required(),
-  });
-
 export const RegisterPage: React.FC = () => {
-  const form = useForm<RegistrationFormData>({
-    resolver: yupResolver(registrationFormSchema),
-    mode: 'onTouched',
-  });
-  const errors = form.formState.errors;
-
-  const onSubmit = form.handleSubmit((data) => {
-    authService
-      .register({ email: data.email, password: data.password })
-      .then(() => console.log('Registration successful'))
-      .catch((error) => console.error('Registration failed: ', error));
-  });
-
   return (
     <Page title="Register">
-      <Form onSubmit={onSubmit}>
-        <Stack gap={2}>
-          <Form.Group controlId="email">
-            <Form.Label>E-mail</Form.Label>
-            <Form.Control
-              type="text"
-              {...form.register('email')}
-              isInvalid={Boolean(errors.email)}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.email?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              {...form.register('password')}
-              isInvalid={Boolean(errors.password)}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.password?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button type="submit">Register</Button>
-        </Stack>
-      </Form>
+      <RegisterForm />
     </Page>
   );
 };
