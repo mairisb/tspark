71b188b7bde1967aa79d5cb5130fc4e7c02826b2
mairisb <berzinsmairis@gmail.com>
2023-08-06 21:20:46 -0700

    redirect to home when user logs in on login page

--- packages/client/src/pages/login/login.page.spec.tsx
+++ packages/client/src/pages/login/login.page.spec.tsx
@@ -1,10 +1,57 @@
+import { configureStore } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
-
+import * as ReactRedux from 'react-redux';
+import * as ReactRouterDom from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { authReducer } from '../../features/auth';
 import { LoginPage } from './login.page';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+const renderWithStore = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+  const store = configureStore({
+    reducer: {
+      auth: authReducer,
+    },
+    preloadedState: {
+      auth: {
+        isLoggedIn,
+        user: null,
+        loading: false,
+        error: null,
+      },
+    },
+  });
+
+  return render(
+    <ReactRedux.Provider store={store}>
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    </ReactRedux.Provider>
+  );
+};
+
 describe('LoginPage', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<LoginPage />);
-    expect(baseElement).toBeTruthy();
+  let navigate: jest.Mock;
+
+  beforeEach(() => {
+    navigate = jest.fn();
+    (ReactRouterDom.useNavigate as jest.Mock).mockReturnValue(navigate);
+  });
+
+  it('should stay in login page if user is not logged in', () => {
+    renderWithStore({ isLoggedIn: false });
+
+    expect(navigate).toHaveBeenCalledTimes(0);
+  });
+
+  it('should redirect to home if the user is logged in', () => {
+    renderWithStore({ isLoggedIn: true });
+
+    expect(navigate).toHaveBeenCalledWith('/');
   });
 });
--- packages/client/src/pages/login/login.page.tsx
+++ packages/client/src/pages/login/login.page.tsx
@@ -1,18 +1,23 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { authSelectors } from '../../features/auth';
 import { LoginForm } from '../../features/auth/components';
 import { Page } from '../page';
 
 export const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
     <Page title="Login">
-      <pre>
-        {JSON.stringify(useSelector(authSelectors.selectAuth), null, '  ')}
-      </pre>
-
       <LoginForm />
-
       <Link to="/register">Register</Link>
     </Page>
   );
