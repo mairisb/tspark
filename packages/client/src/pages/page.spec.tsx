e14a45c23e75a8681ff10252c2fa23a58db248b5
mairisb <berzinsmairis@gmail.com>
2023-08-06 22:32:44 -0700

    change document title based on page title

--- packages/client/src/pages/page.spec.tsx
+++ packages/client/src/pages/page.spec.tsx
@@ -1,10 +1,35 @@
-import { render } from '@testing-library/react';
-
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import { Page } from './page';
 
+jest.mock('../config/config', () => ({
+  config: {
+    APP_NAME: 'MockAppName',
+  },
+}));
+
 describe('Page', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Page title="Test title" />);
-    expect(baseElement).toBeTruthy();
+  it('should render page title', () => {
+    render(<Page title="Test title" />);
+    const pageTitle = screen.getByTestId('page-title');
+
+    expect(pageTitle.textContent).toEqual('Test title');
+  });
+
+  it('should set document title on mount and reset on unmount', () => {
+    const { unmount } = render(<Page title="Test title" />);
+    expect(document.title).toEqual('MockAppName - Test title');
+    unmount();
+    expect(document.title).toEqual('MockAppName');
+  });
+
+  it('should render children', () => {
+    render(
+      <Page title="Test title">
+        <span>Test child</span>
+      </Page>
+    );
+
+    expect(screen.getByText('Test child')).toBeInTheDocument();
   });
 });
--- packages/client/src/pages/page.tsx
+++ packages/client/src/pages/page.tsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
+import { config } from '../config/config';
+
 export interface PageProps extends React.PropsWithChildren {
   title: string;
 }
 
 export const Page: React.FC<PageProps> = (props) => {
+  useEffect(() => {
+    document.title = `${config.APP_NAME} - ${props.title}`;
+    return () => {
+      document.title = config.APP_NAME;
+    };
+  }, [props.title]);
+
   return (
     <main>
-      <h1>{props.title}</h1>
+      <h1 data-testid="page-title">{props.title}</h1>
       {props.children && <main>{props.children}</main>}
     </main>
   );
