dbebe9a770e5043ce0e2bf63921055b697bb976e
mairisb <berzinsmairis@gmail.com>
2023-08-11 13:27:35 -0700

    fix env var parsing in the frontend

--- packages/client/src/core/config.ts
+++ packages/client/src/core/config.ts
@@ -1,14 +1,12 @@
-// const getEnvVar = (key: string): string => {
-//   const value = process.env[key];
-//   if (typeof value === 'undefined') {
-//     throw new Error(`Environment variable ${key} is not defined`);
-//   }
-//   return value;
-// };
+const getEnvVar = (key: string): string => {
+  const value = process.env[key];
+  if (typeof value === 'undefined') {
+    throw new Error(`Environment variable ${key} is not defined`);
+  }
+  return value;
+};
 
 export const config = {
-  // APP_NAME: getEnvVar('NX_APP_NAME'),
-  // API_URL: getEnvVar('NX_API_URL'),
-  APP_NAME: "TSpark",
-  API_URL: "http://localhost:3333/api",
+  APP_NAME: getEnvVar('APP_NAME'),
+  API_URL: getEnvVar('API_URL'),
 };
--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,5 +1,9 @@
-const path = require('path');
+const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const env = dotenv.config().parsed;
 
 module.exports = {
   mode: "development",
@@ -19,6 +23,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(env)
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       favicon: 'src/favicon.ico',
