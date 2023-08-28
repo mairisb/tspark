eda17f05009b6ac8b0a6e9fba00e6eb9d96f3c6b
mairisb <berzinsmairis@gmail.com>
2023-08-27 21:31:13 -0700

    parse int env variables

--- packages/api/src/core/config.ts
+++ packages/api/src/core/config.ts
@@ -2,7 +2,7 @@ import { getEnvVar } from '@tspark/common';
 
 export const config = {
   APP_NAME: getEnvVar('APP_NAME'),
-  PORT: getEnvVar('PORT'),
-  SALT_ROUNDS: getEnvVar('SALT_ROUNDS'),
+  PORT: parseInt(getEnvVar('PORT')),
+  SALT_ROUNDS: parseInt(getEnvVar('SALT_ROUNDS')),
   JWT_SECRET: getEnvVar('JWT_SECRET'),
 };
