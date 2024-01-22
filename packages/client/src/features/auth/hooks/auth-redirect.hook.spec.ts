4ce594f91cdd1115581e2031291a27fafe9eb14e
Mairis Berzins <berzinsmairis@gmail.com>
2024-01-22 22:01:22 +0200

    auth-redirect hook test fix

--- packages/client/src/features/auth/hooks/auth-redirect.hook.spec.ts
+++ packages/client/src/features/auth/hooks/auth-redirect.hook.spec.ts
@@ -1,9 +1,16 @@
 import { renderHook } from '@testing-library/react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthRedirect } from './auth-redirect.hook';
+import { RootStore, useRootStore } from '../../../core/root.store';
+import { AuthStore } from '../auth.store';
 
 jest.mock('react-router-dom', () => ({
   useNavigate: jest.fn(),
+}));
+
+jest.mock('../../../core/root.store', () => ({
+  ...jest.requireActual('../../../core/root.store'),
+  useRootStore: jest.fn(),
 }));
 
 describe('useAuthRedirect', () => {
@@ -15,12 +22,20 @@ describe('useAuthRedirect', () => {
   });
 
   it('redirects to the specified path when the user is logged in', () => {
+    const rootStore = new RootStore();
+    rootStore.authStore.isAuthenticated = true;
+    (useRootStore as jest.Mock).mockReturnValue(rootStore);
+
     renderHook(() => useAuthRedirect('/test'));
 
     expect(navigate).toHaveBeenCalledWith('/test');
   });
 
   it('does not redirect when the user is not logged in', () => {
+    const rootStore = new RootStore();
+    rootStore.authStore.isAuthenticated = false;
+    (useRootStore as jest.Mock).mockReturnValue(rootStore);
+
     renderHook(() => useAuthRedirect('/test'));
 
     expect(navigate).not.toHaveBeenCalled();
