130d401db8d8fa7e8ae03c613ca492e01ca84c2c
Mairis Berzins <berzinsmairis@gmail.com>
2024-01-22 21:35:31 +0200

    remove msw usage

--- packages/client/jest.setup.ts
+++ packages/client/jest.setup.ts
@@ -1,10 +1,4 @@
-import { server } from './src/mocks/server';
-
 process.env = {
   TSPARK_APP_APP_NAME: 'TSpark',
   TSPARK_APP_API_URL: '',
 };
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
