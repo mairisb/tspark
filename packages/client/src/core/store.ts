711386f364a6df4f2318ba335941304aa02e16d5
Mairis Berzins <berzinsmairis@gmail.com>
2024-08-03 11:05:23 +0300

    redux store

---
+++ packages/client/src/core/store.ts
@@ -0,0 +1,35 @@
+import {
+  Action,
+  PreloadedState,
+  ThunkAction,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
+import { authReducer } from '../features/auth/auth.slice';
+import { api } from './api/api';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  [api.reducerPath]: api.reducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+  });
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppStore = ReturnType<typeof setupStore>;
+
+export type AppDispatch = AppStore['dispatch'];
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
