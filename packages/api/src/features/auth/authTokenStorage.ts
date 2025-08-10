d899413dd8b3ac0614c1a2701c36b3cca0cc06fc
Mairis Berzins <berzinsmairis@gmail.com>
2025-08-10 15:06:56 +0300

    remove unused file

--- packages/api/src/features/auth/authTokenStorage.ts
+++
@@ -1,7 +0,0 @@
-let _authToken: string | null = null;
-
-export const authTokenStorage = {
-  get: () => _authToken,
-  set: (token: string) => (_authToken = token),
-  clear: () => (_authToken = null),
-};
